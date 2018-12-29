243d6706b92971462f846c7eba9d2aa76edd32aa
akifo <akiho@bb07.net>
2018-12-29 21:23:38 +0900

    first-2

--- dist/script.js
+++ dist/script.js
@@ -1,14 +1,6 @@
-console.log('aaa');
-
-document
-  .getElementById('add')
-  .addEventListener('click', function () {
-    const a = document.getElementById('a').value
-    const b = document.getElementById('b').value
-    const aa = Number(a)
-    const bb = Number(b)
-
-    console.log(aa, bb);
-    document.getElementById('answer').innerHTML = aa + bb
-  })
-
+var app6 = new Vue({
+  el: '#app-6',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
